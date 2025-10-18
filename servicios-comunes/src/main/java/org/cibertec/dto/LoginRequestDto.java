042b94c1912cc7638800e17da889f3b94a098f57
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-10-18 15:00:17 -0500

    feat: move CORS config from Autenticacion module to ApiGateway YAML

--- servicios-comunes/src/main/java/org/cibertec/dto/LoginRequestDto.java
+++ servicios-comunes/src/main/java/org/cibertec/dto/LoginRequestDto.java
@@ -1,10 +1,14 @@
 package org.cibertec.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
     private String username;
     private String password;
--- servicios-comunes/src/main/java/org/cibertec/dto/LoginResponseDto.java
+++ servicios-comunes/src/main/java/org/cibertec/dto/LoginResponseDto.java
@@ -1,11 +1,15 @@
 package org.cibertec.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginResponseDto {
     private String token;
     private String username;
