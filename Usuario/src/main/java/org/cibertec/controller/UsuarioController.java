e89a40318e7c7be2bb9514905003d7a25df14df0
Estefany Cruz <estefanixzzz@gmail.com>
2025-10-24 03:09:23 -0500

    Agregando circuit breaker y modificando usuario service

--- Usuario/src/main/java/org/cibertec/controller/UsuarioController.java
+++ Usuario/src/main/java/org/cibertec/controller/UsuarioController.java
@@ -1,16 +1,12 @@
 package org.cibertec.controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.cibertec.entity.Rol;
 import org.cibertec.entity.Usuario;
-import org.cibertec.repository.UsuarioRepository;
 import org.cibertec.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,15 +29,12 @@ import lombok.RequiredArgsConstructor;
 public class UsuarioController {
 	@Autowired
     private UsuarioService usuarioService;
-	
-	@Autowired
-	private UsuarioRepository usuarioRepository;
-	
-	BCryptPasswordEncoder pasEncode = new BCryptPasswordEncoder();
 
+	
 	// LISTAR TODOS LOS USUARIOS
     @GetMapping
     public ResponseEntity<List<Usuario>> listaUsuarios() {
+    	
         List<Usuario> usuarios = usuarioService.listarTodos();
         return ResponseEntity.ok(usuarios);
     }
@@ -49,100 +42,64 @@ public class UsuarioController {
     // BUSCAR USUARIO POR ID
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
-        return usuarioService.buscarPorId(id)
+        
+    	
+    	return usuarioService.buscarPorId(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+    
+    
+    // BUSCAR USUARIOS POR ROL
+    @GetMapping("/rol/{idRol}")
+    public ResponseEntity<List<Usuario>> buscarUsuariosPorRol(@PathVariable Long idRol) {
+    	
+    	List<Usuario> usuarios = usuarioService.buscarPorRol(idRol);
+        return ResponseEntity.ok(usuarios);
     }
 
 
     // CREAR NUEVO USUARIO
     @PostMapping
     public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario) {
+    	
     	try {
-	        usuario.setPassword(pasEncode.encode(usuario.getPassword()));
-	        Rol rol=new Rol();
-	        Long nuevoRol=3L;
-	        
-	        if (usuarioRepository.findByUsername(usuario.getUsername()) != null) {
-	        	throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "El usuario ya existe");
-	        }else if (usuarioRepository.findByNumeroDocumento(usuario.getNumeroDocumento()) != null) {
-	        	throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Este documento ya existe");
-	        }else if (usuarioRepository.findByRuc(usuario.getRuc()) != null) {
-	        	throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "El RUC ya existe");
-	        }else if (usuarioRepository.findByCorreo(usuario.getCorreo()) != null) {
-	        	throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "El correo ya existe");
-	        }
-	        if (usuario.getUsername() != null) {
-	        	nuevoRol=2L;
-	        }
-	        rol.setIdRol(nuevoRol);
-	        usuario.setRol(rol);
-
 	        Usuario nuevo = usuarioService.crearUsuario(usuario);
-
 	        return ResponseEntity.ok(nuevo);
 	    } catch (Exception e) {
-	    	throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Algo salio mal al registrar usuario");
+	    	throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Algo salio mal al registrar usuario");
 	    }
     }
 
     // ACTUALIZAR USUARIO
     @PutMapping
-    public ResponseEntity<Usuario> actualizarUsuario( @RequestBody Usuario u) {
-    	Usuario usuario = usuarioService.buscarPorId(u.getIdUsuario())
-    	        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario no encontrado con ID: " + u.getIdUsuario()));
+    public ResponseEntity<Usuario> actualizarUsuario(@RequestBody Usuario usuario) {
     	
-    	Optional<Usuario> username = usuarioRepository.findByUsername(u.getUsername());
-    	if (username.isPresent() && !username.get().getIdUsuario().equals(u.getIdUsuario())) {
-    	    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "El usuario ya existe.");
-    	}
-    	Optional<Usuario> documento = usuarioRepository.findByNumeroDocumento(u.getNumeroDocumento());
-    	if (documento.isPresent() && !documento.get().getIdUsuario().equals(u.getIdUsuario())) {
-    	    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Este documento ya existe.");
-    	}
-    	Optional<Usuario> ruc = usuarioRepository.findByRuc(u.getRuc());
-    	if (ruc.isPresent() && !ruc.get().getIdUsuario().equals(u.getIdUsuario())) {
-    	    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "El RUC ya existe.");
-    	}
-    	Optional<Usuario> correo = usuarioRepository.findByCorreo(u.getCorreo());
-    	if (correo.isPresent() && !correo.get().getIdUsuario().equals(u.getIdUsuario())) {
-    	    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "El correo ya existe.");
-    	}
-    	usuario.setUsername(u.getUsername());
-        usuario.setNumeroDocumento(u.getNumeroDocumento());
-        usuario.setRuc(u.getRuc());
-        usuario.setCorreo(u.getCorreo());
-        usuario.setApellido(u.getApellido());
-        usuario.setDireccion(u.getDireccion());
-        usuario.setNombre(u.getNombre());
-        usuario.setTelefono(u.getTelefono());
-        if (u.getPassword() != null && !u.getPassword().isEmpty()) {
-            usuario.setPassword(pasEncode.encode(u.getPassword()));
-        }
-        
         try {
-            Usuario usuarioActualizado = usuarioService.crearUsuario(usuario); 
+            Usuario usuarioActualizado = usuarioService.actualizarUsuario(usuario); 
             return ResponseEntity.ok(usuarioActualizado);
         } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Algo salió mal al actualizar.");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Algo salió mal al actualizar.");
         }
     }
 
     // ELIMINAR USUARIO
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminarUsuario(@PathVariable Long id) {
-        usuarioService.eliminarUsuario(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<String> eliminarUsuario(@PathVariable Long id) {
+       
+    	
+        String mensaje = usuarioService.eliminarUsuario(id);
+        
+        if(!mensaje.equals("Error")) {	
+        	  return ResponseEntity.ok(mensaje);        	
+        }else {
+        	throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Algo salió mal al eliminar.");
+       	
+        }
+        
     }
 
     
-    // BUSCAR USUARIOS POR ROL
-    @GetMapping("/rol")
-    public ResponseEntity<List<Usuario>> buscarUsuariosPorRol(@RequestParam Long idRol) {
-        Rol rol=new Rol();
-        rol.setIdRol(idRol);
-    	List<Usuario> usuarios = usuarioService.buscarPorRol(rol);
-        return ResponseEntity.ok(usuarios);
-    }
+
 
 }
--- Usuario/src/main/java/org/cibertec/service/UsuarioService.java
+++ Usuario/src/main/java/org/cibertec/service/UsuarioService.java
@@ -4,51 +4,200 @@ package org.cibertec.service;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.cibertec.entity.Rol;
 import org.cibertec.entity.Usuario;
 import org.cibertec.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 
 public class UsuarioService {
+	
 	@Autowired
     private UsuarioRepository usuarioRepository;
 	
+	
+	BCryptPasswordEncoder pasEncode = new BCryptPasswordEncoder();
+	
 
 	// LISTAR TODOS LOS USUARIOS
+	@CircuitBreaker(name = "usuarioService", fallbackMethod = "fallbackListarTodos")
+	@Retry(name="usuarioService")
     public List<Usuario> listarTodos() {
+					
         return usuarioRepository.findAll();
     }
 
     // BUSCAR USUARIO POR ID
+	@CircuitBreaker(name = "usuarioService", fallbackMethod = "fallbackBuscarPorId")
+	@Retry(name="usuarioService")
     public Optional<Usuario> buscarPorId(Long id) {
         return usuarioRepository.findById(id);
+    }
+    
+    
+    // BUSCAR USUARIO POR ROL
+	@CircuitBreaker(name = "usuarioService", fallbackMethod = "fallbackBuscarPorRol")
+	@Retry(name="usuarioService")
+    public List<Usuario> buscarPorRol(Long idRol) {
+        Rol rol=new Rol();
+        rol.setIdRol(idRol);
+        
+        
+        return usuarioRepository.findByRol(rol);
     }
 
 
     // CREAR NUEVO USUARIO
+	@CircuitBreaker(name = "usuarioService", fallbackMethod = "fallbackCrearUsuario")
+	@Retry(name="usuarioService")
     public Usuario crearUsuario(Usuario usuario) {
+    	
+    	
+    	usuario.setPassword(pasEncode.encode(usuario.getPassword()));
+        
+        Rol rol = new Rol();
+        Long idRol=3L;
+        
+        
+        if (usuarioRepository.findByUsername(usuario.getUsername()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El usuario ya existe");
+        }
+        
+        if (usuarioRepository.findByNumeroDocumento(usuario.getNumeroDocumento()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Este documento ya existe");
+        }
+        
+        if (usuario.getRuc() != null && !usuario.getRuc().trim().isEmpty()) {
+            if (usuarioRepository.findByRuc(usuario.getRuc()).isPresent()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El RUC ya existe");
+            }
+            idRol=2L;
+        }
+
+        if (usuarioRepository.findByCorreo(usuario.getCorreo()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El correo ya existe");
+        }
+        
+        rol.setIdRol(idRol);
+        usuario.setRol(rol);
+    	
         return usuarioRepository.save(usuario);
+    }
+    
+	@CircuitBreaker(name = "usuarioService", fallbackMethod = "fallbackActualizarUsuario")
+	@Retry(name="usuarioService")
+    public Usuario actualizarUsuario(Usuario nuevoUsuario) {
+    	
+    	
+    	Usuario usuario = buscarPorId(nuevoUsuario.getIdUsuario())
+    	        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario no encontrado con ID: " + nuevoUsuario.getIdUsuario()));
+    	
+    	Optional<Usuario> username = usuarioRepository.findByUsername(nuevoUsuario.getUsername());
+    	if (username.isPresent() && !username.get().getIdUsuario().equals(nuevoUsuario.getIdUsuario())) {
+    	    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El usuario ya existe.");
+    	}
+    	Optional<Usuario> documento = usuarioRepository.findByNumeroDocumento(nuevoUsuario.getNumeroDocumento());
+    	if (documento.isPresent() && !documento.get().getIdUsuario().equals(nuevoUsuario.getIdUsuario())) {
+    	    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Este documento ya existe.");
+    	}
+    	Optional<Usuario> ruc = usuarioRepository.findByRuc(nuevoUsuario.getRuc());
+    	if (ruc.isPresent() && !ruc.get().getIdUsuario().equals(nuevoUsuario.getIdUsuario())) {
+    	    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El RUC ya existe.");
+    	}
+    	Optional<Usuario> correo = usuarioRepository.findByCorreo(nuevoUsuario.getCorreo());
+    	if (correo.isPresent() && !correo.get().getIdUsuario().equals(nuevoUsuario.getIdUsuario())) {
+    	    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El correo ya existe.");
+    	}
+    	usuario.setUsername(nuevoUsuario.getUsername());
+        usuario.setNumeroDocumento(nuevoUsuario.getNumeroDocumento());
+        usuario.setRuc(nuevoUsuario.getRuc());
+        usuario.setCorreo(nuevoUsuario.getCorreo());
+        usuario.setApellido(nuevoUsuario.getApellido());
+        usuario.setDireccion(nuevoUsuario.getDireccion());
+        usuario.setNombre(nuevoUsuario.getNombre());
+        usuario.setTelefono(nuevoUsuario.getTelefono());
+        if (nuevoUsuario.getPassword() != null && !nuevoUsuario.getPassword().isEmpty()) {
+            usuario.setPassword(pasEncode.encode(nuevoUsuario.getPassword()));
+        }
+        
+        try {
+        	return usuarioRepository.save(usuario);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Algo salió mal al actualizar.");
+        }
+        
     }
 
 
     // ELIMINAR USUARIO
-    public void eliminarUsuario(Long id) {
+	
+	@CircuitBreaker(name = "usuarioService", fallbackMethod = "fallbackEliminarUsuario")
+	@Retry(name="usuarioService")
+    public String eliminarUsuario(Long id) {
         if (usuarioRepository.existsById(id)) {
-            usuarioRepository.deleteById(id);
+        	
+        	 try { 
+        		usuarioRepository.deleteById(id);
+                return "Eliminacion exitosa";
+        		 
+        	 } catch (Exception e) {
+                 throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+             }
+            
         } else {
-            throw new RuntimeException("Usuario no encontrado con id: " + id);
+        	throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
+	
 
-    // BUSCAR USUARIOS POR ROL
-    public List<Usuario> buscarPorRol(Rol rol) {
-        return usuarioRepository.findByRol(rol);
+    
+	/*/*****************************/
+	/********** FallBacks **********/ 
+	/*/*****************************/
+    
+
+	public List<Usuario> fallbackListarTodos(Throwable ex) {
+        System.err.println("Fallback en listar todos los usuarios : " + ex.getMessage());
+        return List.of();
     }
+    
+	public Usuario fallbackBuscarPorId(Long id, Throwable ex) {
+        System.err.println("Fallback en buscar usuario por id="+id + ": "+ ex.getMessage());
+        return new Usuario();
+    }
+    
+	public List<Usuario> fallbackBuscarPorRol(Long idRol, Throwable ex) {
+        System.err.println("Fallback en buscar usuarios por rol id="+ idRol + ": "  + ex.getMessage());
+        return List.of();
+    }
+    
+	public Usuario fallbackCrearUsuario(Usuario usuario, Throwable ex) throws Exception {
+        System.err.println("Fallback en crear usuario: " + ex.getMessage());
+        throw new Exception();
+    }
+    
+	public Usuario fallbackActualizarUsuario(Usuario usuario, Throwable ex) throws Exception {
+        System.err.println("Fallback en actualizar usuario: " + ex.getMessage());
+        throw new Exception();
+    }
+    
+	public String fallbackEliminarUsuario(Long id, Throwable ex) {
+		
+		 System.err.println("Fallback en eliminar el usuario con id=" + id + ": " + ex.getMessage());
+		 return "Error";
+    }
+		
+       
 
 }
