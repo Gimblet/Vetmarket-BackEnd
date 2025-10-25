efbc9edd5937c9f167d9a6c17583ea67119a8890
Gimblet <gimbletdev@gmail.com>
2025-10-24 21:49:41 -0500

    feat: Added Resilience4j for fault tolerance in ProductoService
    - Implemented CircuitBreaker and Retry annotations on service methods.
    - Added fallback methods to handle errors gracefully.

--- Producto/src/main/java/org/cibertec/service/ProductoService.java
+++ Producto/src/main/java/org/cibertec/service/ProductoService.java
@@ -1,10 +1,11 @@
 package org.cibertec.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.cibertec.dto.ProductoRequestDTO;
 import org.cibertec.dto.ProductoResponseDTO;
 import org.cibertec.entity.Producto;
-import org.cibertec.entity.Usuario;
 import org.cibertec.mapper.ProductoMapper;
 import org.cibertec.repository.IProductoRepository;
 import org.cibertec.repository.IUsuarioRepository;
@@ -37,6 +38,8 @@ public class ProductoService {
         this.kafkaPublisherService = kafkaPublisherService;
     }
 
+    @CircuitBreaker(name = "productoGeneral", fallbackMethod = "fallbackGuardar")
+    @Retry(name = "productoGeneral")
     public ResponseEntity<ProductoResponseDTO> guardar(ProductoRequestDTO productoRequestDTO, MultipartFile imagen) {
         if (!usuarioRepository.existsById(productoRequestDTO.getIdUsuario())) {
             return ResponseEntity.notFound().build();
@@ -50,10 +53,10 @@ public class ProductoService {
             else {
                 productoEntidad.setImg(imagen.getBytes());
             }
-            kafkaPublisherService.send(productoRequestDTO);
             productoRepository.save(productoEntidad);
+            kafkaPublisherService.send(productoRequestDTO);
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("Hubo un error al obtener el archivo de imagen: " + e.getMessage());
             throw new RuntimeException(e);
         }
@@ -61,12 +64,16 @@ public class ProductoService {
         return ResponseEntity.ok(productoMapper.toDto(productoEntidad));
     }
 
+    @CircuitBreaker(name = "productoGeneral", fallbackMethod = "fallbackBuscarProductoPorId")
+    @Retry(name = "productoGeneral")
     public ResponseEntity<ProductoResponseDTO> buscarProductoPorID(int id) {
         return ResponseEntity.ok(productoRepository.findById(id)
                 .map(productoMapper::toDto)
                 .orElseThrow(() -> new IllegalArgumentException("No existe el producto con el id: " + id)));
     }
 
+    @CircuitBreaker(name = "productoList", fallbackMethod = "fallbackObtenerProductos")
+    @Retry(name = "productoList")
     public ResponseEntity<List<ProductoResponseDTO>> obtenerProductos() {
         return ResponseEntity.ok(productoRepository.findAll()
                 .stream()
@@ -74,6 +81,8 @@ public class ProductoService {
                 .toList());
     }
 
+    @CircuitBreaker(name = "productoList", fallbackMethod = "fallbackBuscarProductoPorNombre")
+    @Retry(name = "productoList")
     public ResponseEntity<List<ProductoResponseDTO>> buscarProductoPorNombre(String nombre) {
         Optional<List<Producto>> listaProductos = productoRepository.findAllByNombreIgnoreCaseContaining(nombre);
         return listaProductos.map(productos -> ResponseEntity.ok(productos
@@ -82,6 +91,8 @@ public class ProductoService {
                 .toList())).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @CircuitBreaker(name = "productoList", fallbackMethod = "fallbackObtenerProductosPorVeterinario")
+    @Retry(name = "productoList")
     public ResponseEntity<List<ProductoResponseDTO>> obtenerProductosPorVeterinario(Long idVeterinario) {
         Optional<List<Producto>> listaProductos = productoRepository.findAllByUsuario_IdUsuario(idVeterinario);
         return listaProductos.map(productos -> ResponseEntity.ok(productos
@@ -90,6 +101,8 @@ public class ProductoService {
                 .toList())).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @CircuitBreaker(name = "productoGeneral", fallbackMethod = "fallbackActualizarProductoPorId")
+    @Retry(name = "productoGeneral")
     public ResponseEntity<String> actualizarProductoPorId(int id,
                                                           ProductoRequestDTO producto,
                                                           MultipartFile imagen) {
@@ -118,6 +131,8 @@ public class ProductoService {
         }
     }
 
+    @CircuitBreaker(name = "productoGeneral", fallbackMethod = "fallbackRestarStockProducto")
+    @Retry(name = "productoGeneral")
     public ResponseEntity<String> restarStockProducto(int idProducto,
                                                       int stock) {
         Producto producto = productoRepository.findById(idProducto).get();
@@ -132,6 +147,8 @@ public class ProductoService {
                         " actualizado con exito");
     }
 
+    @CircuitBreaker(name = "productoGeneral", fallbackMethod = "fallbackEliminarProductoPorId")
+    @Retry(name = "productoGeneral")
     public ResponseEntity<String> eliminarProductoPorId(int id) {
         if (productoRepository.existsById(id)) {
             productoRepository.deleteById(id);
@@ -141,6 +158,8 @@ public class ProductoService {
         }
     }
 
+    @CircuitBreaker(name = "productoGeneral", fallbackMethod = "fallbackObtenerImagen")
+    @Retry(name = "productoGeneral")
     public ResponseEntity<byte[]> obtenerImagen(int id) {
         Optional<Producto> producto = productoRepository.findById(id);
 
@@ -153,4 +172,103 @@ public class ProductoService {
         }
 
     }
+
+    public ResponseEntity<ProductoResponseDTO> fallbackGuardar(ProductoRequestDTO productoRequestDTO,
+                                                               MultipartFile imagen,
+                                                               Throwable throwable) {
+        System.out.println("FALLBACK: Error al guardar el producto: " + throwable.getMessage());
+        return ResponseEntity.badRequest().body(new ProductoResponseDTO());
+    }
+
+    public ResponseEntity<ProductoResponseDTO> fallbackBuscarProductoPorId(int id, Throwable throwable) {
+        System.out.println("FALLBACK: Error al obtener el producto con id: " + id + " " + throwable.getMessage());
+        return ResponseEntity.notFound().build();
+    }
+
+    public ResponseEntity<List<ProductoResponseDTO>> fallbackObtenerProductos(Throwable throwable) {
+        ProductoResponseDTO productoResponseDTO = new ProductoResponseDTO(
+                0,
+                0.0,
+                0,
+                "ERROR",
+                "No se pudo obtener la lista de productos, intentelo de nuevo mas tarde",
+                Long.valueOf("0"),
+                "",
+                "",
+                "",
+                "",
+                ""
+        );
+        System.out.println("FALLBACK: Error al obtener la lista de productos: " + throwable.getMessage());
+        return ResponseEntity.ok(List.of(productoResponseDTO));
+    }
+
+    public ResponseEntity<List<ProductoResponseDTO>> fallbackBuscarProductoPorNombre(String nombre,
+                                                                                     Throwable throwable) {
+        ProductoResponseDTO productoResponseDTO = new ProductoResponseDTO(
+                0,
+                0.0,
+                0,
+                "ERROR",
+                "No se pudo buscar en la lista de productos, intentelo de nuevo mas tarde",
+                Long.valueOf("0"),
+                "",
+                "",
+                "",
+                "",
+                ""
+        );
+        System.out.println("FALLBACK: Error al intentar buscar la lista de productos: " + throwable.getMessage());
+        return ResponseEntity.ok(List.of(productoResponseDTO));
+    }
+
+    public ResponseEntity<List<ProductoResponseDTO>> fallbackObtenerProductosPorVeterinario(Long idVeterinario,
+                                                                                            Throwable throwable) {
+        ProductoResponseDTO productoResponseDTO = new ProductoResponseDTO(
+                0,
+                0.0,
+                0,
+                "ERROR",
+                "No se pudo obtener en la lista de productos del veterinario, intentelo de nuevo mas tarde",
+                Long.valueOf("0"),
+                "",
+                "",
+                "",
+                "",
+                ""
+        );
+        System.out.println("Fallback: Error al intentar obtener la lista de productos del Veterinario con ID : " + idVeterinario + throwable.getMessage());
+        return ResponseEntity.ok(List.of(productoResponseDTO));
+    }
+
+    public ResponseEntity<String> fallbackActualizarProductoPorId(int id,
+                                                                  ProductoRequestDTO producto,
+                                                                  MultipartFile imagen,
+                                                                  Throwable throwable) {
+        System.out.println("FALLBACK: Error al actualizar el producto con ID : " + id + " " + throwable.getMessage());
+        return ResponseEntity.badRequest().body("Ocurrio un error al intentar actualizar el producto, intentelo de nuevo mas tarde");
+    }
+
+    public ResponseEntity<String> fallbackRestarStockProducto(int idProducto,
+                                                              int stock,
+                                                              Throwable throwable) {
+        System.out.println("FALLBACK: Error al restar " + stock + " del stock del Producto con ID : " +
+                idProducto + " " + throwable.getMessage());
+        return ResponseEntity.badRequest().body("Ocurrio un error al actualizar el stock del producto, intentelo de nuevo mas tarde");
+    }
+
+    public ResponseEntity<String> fallbackEliminarProductoPorId(int id,
+                                                                Throwable throwable) {
+        System.out.println("FALLBACK: Error al eliminar el producto con ID : " +
+                id + " " + throwable.getMessage());
+        return ResponseEntity.badRequest().body("Ocurrio un error al intentar eliminar el producto, intentelo de nuevo mas tarde");
+    }
+
+    public ResponseEntity<byte[]> fallbackObtenerImagen(int id,
+                                                        Throwable throwable) {
+        System.out.println("FALLBACK: Error al obtener la imagen del producto con ID : " +
+                id + " " + throwable.getMessage());
+        return ResponseEntity.notFound().build();
+    }
+
 }
