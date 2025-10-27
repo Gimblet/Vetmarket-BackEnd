207bc896ce9c1fa332fdd383bb163cc630981306
Gimblet <gimbletdev@gmail.com>
2025-10-27 01:28:48 -0500

    fix: Fixed wrong port at docker-run.cmd in Configuraciones

--- Configuraciones/docker-run.cmd
+++ Configuraciones/docker-run.cmd
@@ -2,4 +2,4 @@ REM --- Construcción de la imagen Docker ---
 docker build -f Configuraciones/Dockerfile . -t config-server:1.0
 
 REM --- Ejecución del contenedor ---
-docker run -d --name config-server --network proyectoDAWII-net -p 6001:6000 config-server:1.0
+docker run -d --name config-server --network proyectoDAWII-net -p 8889:8888 config-server:1.0
