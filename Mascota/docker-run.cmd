d3c742f603be0918191af6ddf3afa185c56233ac
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-10-27 18:41:53 -0500

    feat: Add Docker Autenticacion

--- Mascota/docker-run.cmd
+++ Mascota/docker-run.cmd
@@ -2,4 +2,4 @@ REM --- Construcción de la imagen Docker ---
 docker build -t proyectoDAWII/mascota:1.0 -f Mascota/Dockerfile .
 
 REM --- Ejecución del contenedor ---
-docker run -d --name mascota --network proyectoDAWII-net -p 6010:6009 proyectoDAWII/mascota:1.0
+docker run -d --name mascota --network proyectoDAWII -p 6010:6009 proyectoDAWII/mascota:1.0
