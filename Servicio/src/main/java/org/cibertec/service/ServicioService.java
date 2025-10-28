1521ae7dae9e7735ead64a57ab12691ed81b4d97
Dante-Diaz-20 <zevaztian.20@gmail.com>
2025-10-28 15:14:19 -0500

    add CircuitBreakerServicio

--- Servicio/src/main/java/org/cibertec/service/ServicioService.java
+++ Servicio/src/main/java/org/cibertec/service/ServicioService.java
@@ -1,5 +1,7 @@
 package org.cibertec.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.cibertec.dto.ServicioRequestDTO;
 import org.cibertec.dto.ServicioResponseDTO;
@@ -21,20 +23,26 @@ public class ServicioService {
     private final ServicioMapper servicioMapper;
     private final IUsuarioRepository usuarioRepository;
 private final KafkaPublisherService kafkaPublisherService;
-
+// Listar Servicios
+@CircuitBreaker(name = "servicioService", fallbackMethod = "fallbackListarServicio")
+@Retry(name="servicioService")
     public List<ServicioResponseDTO> listarServicio() {
         return servicioRepository.findAll().stream()
                 .map(servicioMapper::toDto)
                 .toList();
     }
-
+// Buscar por Id
+@CircuitBreaker(name = "servicioService", fallbackMethod = "fallbackBuscarServicioPorId")
+    @Retry(name="servicioService")
         public ServicioResponseDTO buscarServicioPorId(Integer id) {
             return servicioRepository.findById(id)
                     .map(servicioMapper::toDto)
                     .orElseThrow(() -> new RuntimeException("No se encontro el servicio con id: " + id));
         }
 
-// Modif repas
+// Crear Servicio
+@CircuitBreaker(name = "servicioService", fallbackMethod = "fallbackCrearServicio")
+@Retry(name="servicioService")
     public ServicioResponseDTO crearServicio(
             String nombre,
             String descripcion,
@@ -58,7 +66,9 @@ private final KafkaPublisherService kafkaPublisherService;
         return servicioMapper.toDto(guardado);
     }
 
-    // Modif repas
+    // Actualizar Servicio
+    @CircuitBreaker(name = "servicioService", fallbackMethod = "fallbackActualizarServicio")
+    @Retry(name="servicioService")
     public ServicioResponseDTO actualizarServicio(
             Integer id,
             String nombre,
@@ -80,25 +90,64 @@ private final KafkaPublisherService kafkaPublisherService;
         Servicio actualizado = servicioRepository.save(servicio);
         return servicioMapper.toDto(actualizado);
     }
-
+// Eliminar Servicio
+@CircuitBreaker(name = "servicioService", fallbackMethod = "fallbackEliminarServicio")
+@Retry(name="servicioService")
         public void eliminarServicio(Integer id) {
             if(!servicioRepository.existsById(id)) {
                 throw new RuntimeException("No se encontro el servicio con id: " + id);
             }
             servicioRepository.deleteById(id);
         }
-
-    // Modif repas
+    // ModifRepas
         public byte[] obtenerImagen(Integer id) {
         return servicioRepository.findById(id)
                 .map(Servicio::getImg)
                 .orElse(null);
         }
-
+// Listar Servicio por Veterinario
+@CircuitBreaker(name = "servicioService", fallbackMethod = "fallbackListarServicioPorVeterinario")
+@Retry(name="servicioService")
     public List<ServicioResponseDTO> listarServiciosPorVeterinario(Long usuarioId) {
         return servicioRepository.findByUsuarioIdUsuario(usuarioId).stream()
                 .map(servicioMapper::toDto)
                 .toList();
     }
+
+    /*/*****************************/
+    /********** FallBacks **********/
+    /*/*****************************/
+//Listar
+    public List<Servicio> fallbackListarServicio(Throwable ex) {
+        System.err.println("Fallback en listar servicio : " + ex.getMessage());
+        return List.of();
+    }
+//BuscarPorId
+    public Servicio fallbackBuscarServicioPorId(Long id, Throwable ex) {
+        System.err.println("Fallback en buscar servicio por id="+id + ": "+ ex.getMessage());
+        return new Servicio();
+    }
+// Crear
+    public Servicio fallbackCrearServicio(Servicio servicio, Throwable ex) throws Exception {
+        System.err.println("Fallback en crear servicio: " + ex.getMessage());
+        throw new Exception();
+    }
+// Actualizar
+    public Usuario fallbackActualizarServicio(Servicio servicio, Throwable ex) throws Exception {
+        System.err.println("Fallback actualizar servicio: " + ex.getMessage());
+        throw new Exception();
+    }
+// Eliminar
+    public String fallbackEliminarServicio(Long id, Throwable ex) {
+
+        System.err.println("Fallback eliminar servicio con id=" + id + ": " + ex.getMessage());
+        return "Error";
+    }
+    // Listar Servicio por Veterinario
+    public List<Servicio> fallbackListarServicioPorVeterinario(Long usuarioId, Throwable ex) {
+        System.err.println("Fallback en buscar usuarios por rol id="+ usuarioId + ": "  + ex.getMessage());
+        return List.of();
+    }
+
     }
 
