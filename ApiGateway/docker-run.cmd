06430475d91050ef4caddc1efe50bd68d4f72596
Gimblet <gimbletdev@gmail.com>
2025-10-28 22:29:53 -0500

    Removed dockerfiles

--- ApiGateway/docker-run.cmd
+++
@@ -1,5 +0,0 @@
-REM --- Construcción de la imagen Docker ---
-docker build -t proyectoDAWII/apigateway:1.0 -f ApiGateway/Dockerfile .
-
-REM --- Ejecución del contenedor ---
-docker run -d --name apigateway --network proyectoDAWII -p 8081:8080 proyectoDAWII/apigateway:1.0
--- Autenticacion/docker-run.cmd
+++
@@ -1,5 +0,0 @@
-REM --- Construcción de la imagen Docker ---
-docker build -t proyectoDAWII/autenticacion:1.0 -f Autenticacion/Dockerfile .
-
-REM --- Ejecución del contenedor ---
-docker run -d --name autenticacion --network proyectoDAWII -p 6008:6007 proyectoDAWII/autenticacion:1.0
